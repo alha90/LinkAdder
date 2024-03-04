4f5d1b24df20bccab932aee531e8bca7d8b0a52e
alha90 <hab.tourist@gmail.com>
2024-03-03 22:37:04 -0600

    Added link removal, though link order is not maintained

--- interactions.js
+++ interactions.js
@@ -1,56 +1,92 @@
 const form = document.getElementById("submit");
 const inputs = document.querySelectorAll("#submit input");
-const ul = document.querySelector(".create_link");
-const clear = document.querySelector(".clear button");
-// let index;
-// console.log('Endale')
-let counter = localStorage.length;
+const ul = document.querySelector("li");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
+  // let counter = localStorage.length;
   let obj = {};
-  Array.from(inputs).forEach((e) => (obj[e.id] = e.value));
-  localStorage.setItem(`data${counter}`, JSON.stringify(obj));
-  storagePersistence(counter);
-  counter += 1;
+  // console.log(counter);
+  Array.from(inputs).forEach((e) => {
+    obj[e.id] = e.value;
+  });
+  localStorage.setItem(obj.description, obj.Link);
+  storagePersistence(obj.description, obj.Link);
+  // counter += 1;
   form.reset();
 });
 
-function storagePersistence(counter) {
+function storagePersistence(description, Link) {
   const link = document.createElement("li");
   const anchor = document.createElement("a");
+  const clearlink = document.createElement("button");
 
   if (localStorage.length != 0) {
-    let descriptionVar = JSON.parse(
-      localStorage.getItem(`data${counter}`)
-    ).description;
-    let linkVar = JSON.parse(localStorage.getItem(`data${counter}`)).Link;
-    anchor.href = linkVar;
+    anchor.href = Link;
     anchor.target = "blank";
-    anchor.setAttribute("id", `${counter}`);
-    anchor.appendChild(document.createTextNode(descriptionVar));
-    link.appendChild(anchor);
+    clearlink.setAttribute("id", description);
+    anchor.appendChild(document.createTextNode(description));
+    clearlink.appendChild(document.createTextNode("remove"));
+    clearlink.classList.add("clear");
+    link.append(anchor, clearlink);
     document.querySelector(".create_link").appendChild(link);
   }
-  return counter;
 }
+
 window.addEventListener("load", (event) => {
-  console.log("inside window before the for loop");
-  if (localStorage.length != 0) {
-    for (var i = 0; i < localStorage.length; i++) {
-      console.log("inside window load");
-      storagePersistence(i);
-    }
-  }
-});
-clear.addEventListener("click", () => {
-  localStorage.clear();
-  location.reload();
+  // for (let index = 0; index < localStorage.length; index++) {
+  //   storagePersistence(index);
+  // }
+  loadElement();
 });
 
 ////////// Function to clear or edit individual links /////////
-function clearEditLink() {
-  for (let index = 0; index < localStorage.length; index++) {}
+
+const removeLink = document.querySelector("#clear");
+removeLink.addEventListener("click", (e) => {
+  e.preventDefault();
+  clearElement();
+});
+
+function clearElement() {
+  const linkClear = document.querySelectorAll("li button");
+
+  linkClear.forEach((e) => {
+    e.classList.toggle("visible");
+    e.addEventListener("click", (event) => {
+      event.preventDefault();
+      console.log(event.target.id);
+      localStorage.removeItem(event.target.id);
+      location.reload();
+    });
+  });
 }
-const clearlink = document.querySelectorAll(selectors);
-function addClearButton() {}
+///////////////////////// End of clear or edit Fucntion ///////////////
+
+///////// Load elements from localStorage ////////
+function loadElement(bool) {
+  assortedData().forEach((e) => {
+    const link = document.createElement("li");
+    const anchor = document.createElement("a");
+    const clearlink = document.createElement("button");
+
+    anchor.href = localStorage.getItem(e);
+    anchor.target = "blank";
+    clearlink.setAttribute("id", e);
+    anchor.appendChild(document.createTextNode(e));
+    clearlink.appendChild(document.createTextNode("remove"));
+    clearlink.classList.add("clear");
+    link.append(anchor, clearlink);
+    document.querySelector(".create_link").appendChild(link);
+  });
+}
+
+function assortedData() {
+  let arr = [];
+  if (localStorage.length != 0) {
+    for (var i = 0; i < localStorage.length; i++) {
+      arr.push(localStorage.key(i));
+    }
+  }
+  return arr;
+}
